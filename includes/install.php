7ee2e2483ab0a10cd43a14d5dc5799440bbd589e
WP Bacon <news@wpbacon.com>
2014-03-23 04:41:06 -0500

    Added a proper install file.
    Moved add_option functions into an activation hook and added a check to
    make sure APC is installed on the server.

---
+++ includes/install.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Install functions.
+ *
+ * @package      WP APC Clear
+ * @author       Robert Neu <http://wpbacon.com/>
+ * @copyright    Copyright (c) 2014, FAT Media, LLC
+ * @license      GPL2+
+ *
+ */
+
+// Exit if accessed directly
+defined( 'WPINC' ) or die;
+
+// Grab the plugin file.
+$_wpapcc_plugin_file = $_wpapcc_dir . 'wp-apc-clear.php';
+
+function wpapcc_is_apc_installed() {
+	if ( extension_loaded( 'apc' ) || ini_get( 'apc.enabled' ) ) {
+		return true;
+	}
+	return fasle;
+}
+
+/**
+ * Install
+ *
+ * Runs on plugin install and checks to make sure Genesis is activated.
+ *
+ * @since 1.0
+ * @return void
+ */
+function wpapcc_install( $_wpapcc_plugin_file ) {
+
+	$is_apc_installed = wpapcc_is_apc_installed();
+
+	// Deactivate the plugin if APC isn't instlaled.
+	if ( ! $is_apc_installed ) {
+		// Deactivate the plugin.
+		deactivate_plugins( $_wpapcc_plugin_file );
+		// Display a reason why the plugin has been deactivated.
+		wp_die( 'Sorry, this plugin requires APC. Please install APC and then activate the plugin again.' );
+	}
+
+	// Create the cache status option if it doesn't already exist.
+	if ( ! get_option( 'wpapcc_cache_status' ) ) {
+		add_option( 'wpapcc_cache_status', 'stale', '', 'no' );
+	}
+
+	// Create the debug status option if it doesn't already exist.
+	if ( ! get_option( 'wpapcc_debug_status' ) ) {
+		add_option( 'wpapcc_debug_status', 'disabled', '', 'no' );
+	}
+}
+register_activation_hook( $_wpapcc_plugin_file, 'wpapcc_install' );
+
+// Clean up
+unset( $_wpapcc_plugin_file );
--- includes/plugin.php
+++ includes/plugin.php
@@ -28,10 +28,6 @@ class WP_APC_Clear {
 	 */
 	private function __construct() {
 		add_action( 'plugins_loaded', array( $this, 'load' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'scripts_styles' ), 10 );
-		add_action( 'admin_menu', array( $this, 'create_admin_page' ) );
-		add_action( 'admin_notices', array( $this,'admin_notices' ) );
-		add_action( 'admin_bar_menu', array( $this, 'toolbar_link' ), 999 );
 	}
 
 	/**
@@ -41,8 +37,9 @@ class WP_APC_Clear {
 	 * @return WP_APC_Clear
 	 */
 	public static function getInstance() {
-		if ( !self::$instance )
+		if ( ! self::$instance ) {
 			self::$instance = new self;
+		}
 		return self::$instance;
 	}
 
@@ -50,16 +47,13 @@ class WP_APC_Clear {
 	 * Load the plugin.
 	 */
 	public function load() {
-
 		self::textdomain();
 		self::define_constants();
-
+		self::hooks();
 	}
 
 	/**
 	 * load textdomain
-	 *
-	 * @return WP_APC_Clear
 	 */
 	public function textdomain() {
 		load_plugin_textdomain( 'wpapcc', false, dirname( plugin_basename( __FILE__ ) ) . '/language/' );
@@ -71,7 +65,7 @@ class WP_APC_Clear {
 	public function define_constants() {
 		// Plugin version.
 		if ( ! defined( 'WPAPCC_VERSION' ) ) {
-			define( 'WPAPCC_VERSION', '0.2.0' );
+			define( 'WPAPCC_VERSION', '0.2.1' );
 		}
 
 		// Plugin root file.
@@ -91,9 +85,19 @@ class WP_APC_Clear {
 	}
 
 	/**
+	 * Hook into WordPress to run actions and filters.
+	 */
+	function hooks() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'scripts_styles' ), 10 );
+		add_action( 'admin_menu', array( $this, 'create_admin_page' ) );
+		add_action( 'admin_notices', array( $this,'admin_notices' ) );
+		add_action( 'admin_bar_menu', array( $this, 'toolbar_link' ), 999 );
+	}
+
+	/**
 	 * Clear the APC cache.
 	 *
-	 * @peram $cache_type the type of cache to clear.
+	 * @param $cache_type the type of cache to clear.
 	 */
 	function apc_clear( $cache_type = '' ) {
 
@@ -144,7 +148,7 @@ class WP_APC_Clear {
 	 * @global $wpapcc_admin_page
 	 */
 	function create_admin_page() {
-		add_management_page( __( 'WP Clear APC', 'wpapcc'), __( 'Manage APC', 'wpapcc'), 'manage_options', 'wp-clear-apc', array( $this, 'apc_clear_page' ) );
+		add_management_page( __( 'WP Clear APC', 'wpapcc'), __( 'Clear APC', 'wpapcc'), 'manage_options', 'wp-clear-apc', array( $this, 'apc_clear_page' ) );
 	}
 
 	/**
@@ -153,7 +157,7 @@ class WP_APC_Clear {
 	 * @return null if user can't manage options.
 	 */
 	function apc_clear_page() {
-
+		// Do nothing if the user can't manage options.
 		if ( ! current_user_can( 'manage_options' ) ) {
 			return;
 		}
@@ -170,10 +174,17 @@ class WP_APC_Clear {
 				<p>
 					<input type="submit" value="<?php _e( 'Clear User Cache', 'wpapcc' ) ?>" class="button button-primary clear-user" name="clear-apc">
 					<input type="submit" value="<?php _e( 'Clear System Cache', 'wpapcc' ) ?>" class="button button-secondary clear-system" name="clear-apc">
+					<?php echo $this->get_debug_toggle(); ?>
 				</p>
 			</form>
-			<?php $this->clear_the_cache(); ?>
-			<?php $this->debug_info(); ?>
+			<?php
+			// Clear the APC cache.
+			$this->clear_the_cache();
+			// Process the debug toggle button.
+			$this->process_debug_toggle();
+			// Show debugging information if it's been enabled
+			$this->maybe_show_debugging();
+			?>
 		</div>
 
 	<?php }
@@ -184,33 +195,30 @@ class WP_APC_Clear {
 	 * @return null if the user hasn't submitted the cache clearing form.
 	 */
 	function clear_the_cache() {
-		if ( !isset( $_POST['clear-apc'] ) ) {
+		// Do nothing if the cache hasn't been cleared.
+		if ( ! isset( $_POST['clear-apc'] ) ) {
 			return;
 		}
-
-		if ( ! get_option( 'wpapcc_cache_status' ) ) {
-			add_option( 'wpapcc_cache_status', 'stale', '', 'no' );
-		}
-
+		// Clear the user cache and update the cache status accordingly.
 		if ( $_POST['clear-apc'] === __( 'Clear User Cache', 'wpapcc' ) ) {
-			update_option( 'wpapcc_cache_status', 'user-cleared' );
 			$this->apc_clear( 'user' );
+			update_option( 'wpapcc_cache_status', 'user-cleared' );
 		}
+		// Clear the system cache and update the cache status accordingly.
 		elseif ( $_POST['clear-apc'] === __( 'Clear System Cache', 'wpapcc' ) ) {
-			update_option( 'wpapcc_cache_status', 'system-cleared' );
 			$this->apc_clear();
+			update_option( 'wpapcc_cache_status', 'system-cleared' );
 		}
 	}
 
 	/**
-	 * Add notices to the admin page to show if the cache has been cleard.
+	 * Add notices to the admin page to show if the cache has been cleared.
 	 *
-	 * @global $wpapcc_admin_page
-	 * @return null if we're not on the plugin page.
+	 * @return null if the user hasn't submitted the cache clearing form.
 	 */
 	function admin_notices() {
 		// Do nothing if we're not on the plugin admin page.
-		if ( ! $this->is_admin_page() || ! isset( $_POST['clear-apc'] ) ) {
+		if ( ! isset( $_POST['clear-apc'] ) ) {
 			return;
 		}
 
@@ -222,13 +230,21 @@ class WP_APC_Clear {
 			$notice .= '<p>' . __( 'Clearing APC has failed!', 'wpapcc' ) . '</p>';
 		$notice .= '</div>';
 
-		// Display a success notice if the cache has been cleared.
-		if ( 'user-cleared' === $cache_status ) {
+		// If the cache status hasn't been updated, end here.
+		if ( 'stale' === $cache_status ) {
+			echo $notice;
+			return;
+		}
+
+		// Display a success notice if the user cache has been cleared.
+		if ( $_POST['clear-apc'] === __( 'Clear User Cache', 'wpapcc' ) ) {
 			$notice = '<div id="message" class="updated">';
 				$notice .= '<p>' . __( 'APC user cache has been cleared!', 'wpapcc' ) . '</p>';
 			$notice .= '</div>';
 		}
-		if ( 'system-cleared' === $cache_status ) {
+
+		// Display a success notice if the system cache has been cleared.
+		if ( $_POST['clear-apc'] === __( 'Clear System Cache', 'wpapcc' ) ) {
 			$notice = '<div id="message" class="updated">';
 				$notice .= '<p>' . __( 'APC system cache has been cleared!', 'wpapcc' ) . '</p>';
 			$notice .= '</div>';
@@ -240,7 +256,88 @@ class WP_APC_Clear {
 		echo $notice;
 	}
 
-	function debug_info() { ?>
+	/**
+	 * Display a debug toggle button.
+	 *
+	 * @return string a button to enable or disable debugging.
+	 */
+	function get_debug_toggle() {
+		// Assign the toggle button statuses to some variables for reuse.
+		$show_info = '<input type="submit" value="' . __( 'Show Debugging Info', 'wpapcc' ) . '" class="button button-secondary show-debug" name="debug-apc">';
+		$hide_info = '<input type="submit" value="' . __( 'Hide Debugging Info', 'wpapcc' ) . '" class="button button-secondary show-debug" name="debug-apc">';
+
+		// Display the show info button when debugging is disabled.
+		$button = $show_info;
+
+		// Display the hide info button when debugging is enabled.
+		if ( get_option( 'wpapcc_debug_status' ) === 'enabled' ) {
+			$button = $hide_info;
+		}
+
+		// If the debugging toggle hasn't been submitted, end here.
+		if ( ! isset( $_POST['debug-apc'] ) ) {
+			return $button;
+		}
+
+		// Display the show info button when debugging is disabled.
+		$button = $show_info;
+
+		// Display the hide info button when debugging is enabled.
+		if ( $_POST['debug-apc'] === __( 'Show Debugging Info', 'wpapcc' ) ) {
+			$button = $hide_info;
+		}
+
+		return $button;
+	}
+
+	/**
+	 * Process the debug toggle button when it's clicked.
+	 *
+	 * @return null if the toggle button hasn't been used.
+	 */
+	function process_debug_toggle() {
+		// Do nothing if the debug status hasn't been changed.
+		if ( ! isset( $_POST['debug-apc'] ) ) {
+			return;
+		}
+
+		if ( $_POST['debug-apc'] === __( 'Show Debugging Info', 'wpapcc' ) ) {
+			update_option( 'wpapcc_debug_status', 'enabled' );
+		}
+		else {
+			update_option( 'wpapcc_debug_status', 'disabled' );
+		}
+	}
+
+	/**
+	 * Display the debugging info only when it's enabled.
+	 *
+	 * @return null if the user has disabled debugging.
+	 */
+	function maybe_show_debugging() {
+		// Do nothing if we're not on the plugin admin page.
+		if ( ! $this->is_admin_page() ) {
+			return;
+		}
+		// Get the current debug status.
+		$debug_status =  get_option( 'wpapcc_debug_status' );
+
+		// Do nothing if debugging has been disabled.
+		if ( 'disabled' === $debug_status ) {
+			return;
+		}
+
+		// Show the APC debugging information.
+		$this->debug_info();
+	}
+
+	/**
+	 * HTML output of the APC cache for debugging purposes.
+	 *
+	 * @uses apc_cache_info().
+	 */
+	function debug_info() {
+		?>
 		<div id="wpapcc_content_top" class="postbox-container">
 			<div class="meta-box-sortables">
 				<h2 id="wpapcc-tabs" class="nav-tab-wrapper">
@@ -273,7 +370,7 @@ class WP_APC_Clear {
 	/**
 	 * Add a link to clear the cache from the admin toolbar.
 	 *
-	 *  $wp_admin_bar
+	 *  @uses  $wp_admin_bar
 	 */
 	function toolbar_link( $wp_admin_bar ) {
 
@@ -282,12 +379,12 @@ class WP_APC_Clear {
 		}
 
 		$args = array(
-			'id'    => 'manage-apc',
-			'title' => 'Manage APC',
+			'id'    => 'clear-apc',
+			'title' => 'Clear APC',
 			'href'  => '/wp-admin/tools.php?page=wp-clear-apc',
 			'meta'  => array(
-				'class' => 'manage-apc',
-				'title' => 'Manage APC'
+				'class' => 'clear-apc',
+				'title' => 'Clear APC'
 			)
 		);
 
